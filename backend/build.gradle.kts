0eef2ab32759b53098bdcfecf692906b532c2245
ropurt <ropurt@taltech.ee>
2026-05-26 16:21:07 +0300

    first flyway sql to the db

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
