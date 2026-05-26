63fbaeb19cd8ade91e3e67f5dc15a2898e494b63
ropurt <ropurt@taltech.ee>
2026-05-26 16:11:24 +0300

    backend init done, java, springboot, docker

---
+++ backend/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.stay"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ backend/src/main/java/com/stay/StayApplication.java
@@ -0,0 +1,11 @@
+package com.stay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StayApplication.class, args);
+    }
+}
